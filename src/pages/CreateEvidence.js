9859da8015b8c11c4f04ef7642dbf7eb29378884
Shritesh <shritesh.sj@gmail.com>
2020-10-29 21:34:38 +0530

    Completed IPFS logic

--- src/pages/CreateEvidence.js
+++ src/pages/CreateEvidence.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import Title from '../components/Title'
-
+import React from "react";
+import Title from "../components/Title";
+import { IPFS } from "../services";
 // have to add file support
 
 function getFormattedDate(date) {
-    let year = date.getFullYear();
-    let month = (1 + date.getMonth()).toString().padStart(2, '0');
-    let day = date.getDate().toString().padStart(2, '0');
-  
-    return month + '-' + day + '-' + year;
+	let year = date.getFullYear();
+	let month = (1 + date.getMonth()).toString().padStart(2, "0");
+	let day = date.getDate().toString().padStart(2, "0");
+
+	return month + "-" + day + "-" + year;
 }
 
 class CreateEvidence extends React.Component {
-	constructor(props){
+	constructor(props) {
 		super(props);
 
 		this.state = {
-			name:'',
-			key:'',
-			user:'',
-			details:''
-		}
+			name: "",
+			key: "",
+			user: "",
+			details: "",
+			file: null,
+			cid: "",
+		};
 
 		this.handleEviNameChange = this.handleEviNameChange.bind(this);
 		this.handleDetailsChange = this.handleDetailsChange.bind(this);
@@ -34,38 +36,38 @@ class CreateEvidence extends React.Component {
 	}
 
 	handleEviNameChange(event) {
-		this.setState({name: event.target.value});
+		this.setState({ name: event.target.value });
 	}
 
 	handleDetailsChange(event) {
-		this.setState({details: event.target.value});
+		this.setState({ details: event.target.value });
 	}
 
-	handleSubmit(event) {
+	async handleSubmit(event) {
 		event.preventDefault();
 
 		let date = new Date();
 
 		this.setState({
-			key: getFormattedDate(date), 	// shown as Date Added in the Evidence List (mm-dd-yyyy format)
-			user:''							// user data to be taken from database
-		})
+			key: getFormattedDate(date), // shown as Date Added in the Evidence List (mm-dd-yyyy format)
+			user: "", // user data to be taken from database
+		});
 
-		alert(`Successfully submitted evidence! Selected file - ${this.fileInput.current.files[0].name}`);
+		if (this.fileInput.current.files.length !== 0) {
+			const cid = await IPFS.addFile(this.fileInput.current.files[0]);
+			this.setState({ cid });
+		} else console.log("No file uploaded");
 	}
 
 	render() {
-		return(
+		return (
 			<div className="tc pa3">
-
 				<Title />
 
-				<div className='dib b--solid bw1 b--moon-gray mt5 pa3 br4 bg-black-025'>
-
+				<div className="dib b--solid bw1 b--moon-gray mt5 pa3 br4 bg-black-025">
 					<h1>Submit an Evidence</h1>
 
 					<form onSubmit={this.handleSubmit}>
-
 						<input
 							className="pa2 ma3 br4"
 							type="text"
@@ -87,12 +89,11 @@ class CreateEvidence extends React.Component {
 							<h4 className="mt4">Upload file: </h4>
 							<p>(preferably pdf or jpg)</p>
 
-							<input 
+							<input
 								className="tc"
 								type="file"
 								ref={this.fileInput}
 							/>
-
 						</label>
 						<br />
 
@@ -101,7 +102,12 @@ class CreateEvidence extends React.Component {
 							type="submit"
 							value="Submit Evidence"
 						/>
-						
+						{this.state.cid !== "" ? (
+							<a
+								href={`${process.env.REACT_APP_IPFS_BASE_URL}${this.state.cid}`}>
+								Download
+							</a>
+						) : null}
 					</form>
 				</div>
 			</div>
--- src/services/index.js
+++ src/services/index.js
@@ -3,5 +3,6 @@ import Signing from "./signing";
 import Transaction from "./transaction";
 import Requests from "./requests";
 import Payload from "./payload";
+import IPFS from "./ipfs";
 
-export { Utils, Signing, Transaction, Requests, Payload };
+export { Utils, Signing, Transaction, Requests, Payload, IPFS };
